57207c538ace82901027baa20f24b9a3fabdd1b1
Ampy Fejes <afejes@apple.com>
2016-09-26 16:53:31 -0700

    Initial Commit

---
+++ BorderlessButton/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BorderlessButton
+//
+//  Created by Ampy Fejes on 9/26/16.
+//  Copyright © 2016 Touch of Cocoa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BorderlessButton/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BorderlessButton
+//
+//  Created by Ampy Fejes on 9/26/16.
+//  Copyright © 2016 Touch of Cocoa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
